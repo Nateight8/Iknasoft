073beeb4b36e0f994dc9b64914c30ef7fd02d8e8
Mbaocha Jonathan <mbaocha240793@gmail.com>
2025-08-15 14:25:31 +0100

    feat: restructure layout and add UI components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/navigation/app-sidebar";
+import { AppBar } from "@/components/navigation/appbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,11 +34,24 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SidebarProvider>
-          <AppSidebar />
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 72)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar variant="inset" />
           <SidebarInset>
-            <SidebarTrigger />
-            {children}
+            <AppBar />
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                  {children}
+                </div>
+              </div>
+            </div>
           </SidebarInset>
         </SidebarProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+// import { AppSidebar } from "@/components/app-sidebar";
+// import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+// import { DataTable } from "@/components/data-table";
+// import { SectionCards } from "@/components/section-cards";
+// import { SiteHeader } from "@/components/site-header";
+import { AppSidebar } from "@/components/navigation/app-sidebar";
+import { AppBar } from "@/components/navigation/appbar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+// import data from "./data.json";
+
 export default function Page() {
-  return <div className="">hello world</div>;
+  return <div className="">hello data</div>;
 }
